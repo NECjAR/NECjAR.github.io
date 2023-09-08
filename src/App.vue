477aa18bd6912194066f09322a5fa1fc5fc74665
Kadae <kadae@yandex.ru>
2023-09-09 00:04:57 +0500

    Vue, baby.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from './components/Footer.vue'
+import Navigation from './components/Navigation.vue'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+header,
+footer {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <img alt="NECjAR logo" class="logo" src="@/assets/icons/icon-kadae.jpg" width="350" height="280" />
+    <p>NECjAR 2024, Coming soon</p>
+  </main>
+</template>
