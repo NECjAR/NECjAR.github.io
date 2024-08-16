7ccbbdacdc25a8df0d87526f2bd4dd86ce9a1398
Kadae <Kadae@users.noreply.github.com>
2024-08-16 17:54:40 +0500

    Updates

--- _nuxt/DWil1SPs.js
+++
@@ -1,1 +0,0 @@
-const t=()=>({legacy:!1,locale:"en",messages:{en:{titleDonate:"Donate",titlePolicy:"Policy",titleSearch:"Search",titleStore:"Store",titleSun:"Sun"},ru:{titleDonate:"Донат",titlePolicy:"Политика",titleSearch:"Поиск",titleStore:"Магазин",titleSun:"Солнце"}}});export{t as default};
